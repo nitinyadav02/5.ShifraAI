45de46b84d3e4a57572d1c6ff3cad3beebbf70f0
nitinyadav02 <ny43554@gmail.com>
2026-06-28 13:56:33 +0530

    Modify cookie options for token authentication
    Updated cookie settings for token to enhance security.

--- Server/Controllers/auth.controller.js
+++ Server/Controllers/auth.controller.js
@@ -15,9 +15,9 @@ export const googleAuth = async (req,res) => {
         }
         const token = await genToken(user._id)
         res.cookie("token" , token , {
-            httpOnly:true,
-            secure:false,
-            sameSite:"strict",
+            httpOnly:false,
+            secure:true,
+            sameSite:"none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         } )
 
